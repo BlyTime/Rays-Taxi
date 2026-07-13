4e1e892573638a0cba6fb511c83ec76fdc899759
BlyTime <ray2yaw@gmail.com>
2026-07-12 21:17:46 -0400

    Add Firebase configuration and initialization

--- firebase.js
+++ firebase.js
@@ -1,1 +1,48 @@
 
+// Firebase Configuration
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+
+import {
+    getDatabase,
+    ref,
+    push,
+    set,
+    onValue
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-database.js";
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyCkxcHldzvMJun6lAcZuI5ZwKr1fsmjYv8",
+
+    authDomain: "rays-taxi.firebaseapp.com",
+
+    databaseURL: "https://rays-taxi-default-rtdb.firebaseio.com",
+
+    projectId: "rays-taxi",
+
+    storageBucket: "rays-taxi.firebasestorage.app",
+
+    messagingSenderId: "676032335281",
+
+    appId: "1:676032335281:web:e20b5063db12bdc3f18190"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+const database = getDatabase(app);
+
+export {
+
+    database,
+
+    ref,
+
+    push,
+
+    set,
+
+    onValue
+
+};
