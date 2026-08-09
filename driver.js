1a4c2ddbec4702d3755c93624fd08273f4839876
BlyTime <ray2yaw@gmail.com>
2026-08-08 23:47:12 -0400

    Change oscillator type to square and adjust gain settings

--- driver.js
+++ driver.js
@@ -49,11 +49,11 @@ function playNewRequestSound() {
     [0, 0.22].forEach((delay, index) => {
         const oscillator = audioContext.createOscillator();
         const gain = audioContext.createGain();
-        oscillator.type = "sine";
+        oscillator.type = "square";
         oscillator.frequency.value = index === 0 ? 880 : 1175;
-        gain.gain.setValueAtTime(0.1, audioContext.currentTime + delay);
-        gain.gain.exponentialRampToValueAtTime(1, audioContext.currentTime + delay + 0.02);
-        gain.gain.exponentialRampToValueAtTime(0.5, audioContext.currentTime + delay + 0.16);
+        gain.gain.setValueAtTime(0.2, audioContext.currentTime + delay);
+        gain.gain.exponentialRampToValueAtTime(3, audioContext.currentTime + delay + 0.02);
+        gain.gain.exponentialRampToValueAtTime(0.0001, audioContext.currentTime + delay + 0.16);
         oscillator.connect(gain).connect(audioContext.destination);
         oscillator.start(audioContext.currentTime + delay);
         oscillator.stop(audioContext.currentTime + delay + 0.17);
